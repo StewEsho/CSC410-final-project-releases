d24b83a611477f49884a1469933cae11b189ab70
StewEsho <stewardesho@gmail.com>
2021-12-10 02:31:47 -0500

    Added verification

--- verification/verifier.py
+++ verification/verifier.py
@@ -9,13 +9,109 @@ of the assignment.
 from z3 import *
 from lang.ast import *
 
+class AstToZ3:
+    def __init__(self) -> None:
+        pass
+
+    def convert(self, formula: Expression) -> ExprRef:
+        """
+        Converts the format of an AST expression 
+        from lang.ast.Expression
+        to z3.ExprRef.
+
+        This allows z3 to attempt to solve the expression,
+        and thus verify the expression.
+        """
+        print(f'---> {str(formula)}')
+
+        result = None
+
+        # Case 1 : formula is a binary expression
+        if isinstance(formula, BinaryExpr):
+            operator = pythonize(str(formula.operator))
+            lhs = self.convert(formula.left_operand)
+            rhs = self.convert(formula.right_operand)
+
+            op_to_func = {
+                "and": And,
+                "or": Or
+            }
+
+            # TODO: check if lhs and rhs are of wrong type
+            if operator in op_to_func:
+                result = op_to_func[operator](lhs, rhs)
+            else:
+                result = eval(f"lhs {operator} rhs")
+
+
+        # Case 2 : formula is a unary expression.
+        elif isinstance(formula, UnaryExpr):
+            operand = self.convert(formula.operand)
+
+            if formula.operator == UnaryOperator.NOT:
+                result = Not(operand)
+            elif formula.operator == UnaryOperator.ABS:
+                result = If(operand < 0, -operand, operand)
+            elif formula.operator == UnaryOperator.NEG: 
+                result = -operand
+            else:
+                result = None
+
+        # Case 3 : formula is a if-then-else expression (a ternary expression).
+        elif isinstance(formula, Ite):
+            cond = self.convert(formula.cond)
+            true_branch = self.convert(formula.true_br)
+            false_branch = self.convert(formula.false_br)
+
+            result = If(cond, true_branch, false_branch)
+
+        # Case 4: formula is a variable
+        elif isinstance(formula, VarExpr):
+            # TODO: clean up by using type->function dictionary
+            if formula.var.type == PaddleType.INT:
+                result = Int(formula.var.name)
+            elif formula.var.type == PaddleType.BOOL:
+                result = Bool(formula.var.name)
+            else:
+                None #TODO: add better edge case here
+
+        # Case 5 : formula i a boolean constant
+        elif isinstance(formula, (BoolConst, IntConst)):
+            result = formula.value
+
+        # Case 6 : formula is GrammarInteger or GramamrVar: this should
+        # never happen during evaluation!
+        elif isinstance(formula, (GrammarInteger, GrammarVar)):
+            raise EvaluationTypeError(
+                "GrammarInteger and GrammarVar should not appear in\
+                    expressions that are evaluated.")
+
+        # Case 7 should never be reached.
+        elif isinstance(formula, Expression):
+            raise EvaluationTypeError(
+                "Argument is an Expression of unknown type!\n\
+                    Maybe you forgot to implement a case in \
+                        verifier.expression_to_z3")
+        
+        return result
 
 def is_valid(formula: Expression) -> bool:
     """
     Returns true if the formula is valid.
-
     """
-    # TODO: implement this function.
-    # It should return true if the formula is valid.
-    # To check that the formula is valid, you should use the Z3 api
-    raise Exception("verifier.is_valid is not implemented.")
+    z3_converter = AstToZ3()
+    z3_formula = z3_converter.convert(formula)
+    s=Solver()
+    s.add(z3_formula)
+    return s.check() == sat
+
+# if __name__ == "__main__":
+#     x = VarExpr(Variable("x", PaddleType.INT))
+#     y = VarExpr(Variable("y", PaddleType.INT))
+#     x_gt_y = BinaryExpr(BinaryOperator.GREATER, x, y)
+#     # y_gt_3 = BinaryExpr(BinaryOperator.GREATER, y, IntConst(3))
+#     y_gt_3 = BinaryExpr(BinaryOperator.GREATER, y, x)
+#     full_expr = BinaryExpr(BinaryOperator.AND, x_gt_y, y_gt_3)
+#     print(str(full_expr))
+#     print('-------------------')
+#     print(is_valid(full_expr))
