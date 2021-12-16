6ac691ce7f62d23b86f3ecbdae01ca6878a065f7
re-mint <babak.eric@gmail.com>
2021-12-15 22:44:06 -0500

    tests for q2

--- test/student_test.py
+++ test/student_test.py
@@ -156,7 +156,6 @@ class TestStudent(unittest.TestCase):
         self.assertEqual(len(prog.inputs), 7,
             msg="In %s, we expected exactly 7 inputs." % filename)
 
-        prog_res = empty.evaluate(prog)
         # The result should be an expression
         self.assertIsInstance(prog_res, Expression)
         # In this particular case, the expression should be a binary expression
